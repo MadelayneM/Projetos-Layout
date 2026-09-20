32a4d4c8363cda755865ded2369e96b8cd8478e9
Altairi40 <lucas.shiraoka@gmail.com>
2026-09-20 14:05:33 -0300

    Layout Poupança Pronto

--- Projeto-Poupanca/app/src/main/java/com/example/projetopoupanca/MainActivity.java
+++ Projeto-Poupanca/app/src/main/java/com/example/projetopoupanca/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.projetopoupanca;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etValorInicial, etAplicacaoMensal, etTempoAplicacao, etTaxa;
+    private Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        etValorInicial = findViewById(R.id.etValorInicial);
+        etAplicacaoMensal = findViewById(R.id.etAplicacaoMensal);
+        etTempoAplicacao = findViewById(R.id.etTempoAplicacao);
+        etTaxa = findViewById(R.id.etTaxa);
+        btnCalcular = findViewById(R.id.btnCalcular);
+
+
+
+        btnCalcular.setOnClickListener(v -> {
+            String valorInicial = etValorInicial.getText().toString().trim();
+            String apliccaoMensal = etAplicacaoMensal.getText().toString().trim();
+            String tempoAplicacao = etTempoAplicacao.getText().toString().trim();
+            String taxa = etTaxa.getText().toString().trim();
+
+            if(!valorInicial.isEmpty() && !apliccaoMensal.isEmpty() && !tempoAplicacao.isEmpty() && !taxa.isEmpty()){
+                Toast.makeText(this,"Operação realizada com sucesso!!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Campo Vazio!! Digite um valor", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
